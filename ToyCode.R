d94cb1a04b2f5f1db2d61dcdb890fde4a062ff07
MollieZ <molliezhu@hotmail.com>
2018-06-15 21:40:40 +1000

    Plot

--- ToyCode.R
+++ ToyCode.R
@@ -3,6 +3,7 @@
 
 ###Library
 library(xlsx)
+library(ggplot2)
 library(lubridate)
 
 #Impport Data
@@ -11,3 +12,7 @@ str(RawData)
 summary(RawData)
 PlannedData<-read.xlsx("toy_sales_data.xlsx",2,header = T)
 
+ggplot(RawData, aes(month)) + 
+  geom_line(aes(y = sales, colour = "Sales")) + 
+  geom_line(aes(y = tv_spend, colour = "TV Investment")) +
+  geom_line(aes(y = digital_spend, colour = "Digital Investment"))
