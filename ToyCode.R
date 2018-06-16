4046cf5875e9cd8e3f64bc4a8b9230578477f21a
MollieZ <molliezhu@hotmail.com>
2018-06-16 22:52:13 +1000

    PPT 1

--- ToyCode.R
+++ ToyCode.R
@@ -47,7 +47,54 @@ PlannedData$xmas<-0
 predict(Model1, PlannedData, interval="predict") 
 
 
-Model2<-lm(sales ~ tv_spend + digital_spend +trend + xmas-1,RawData)
+
+#######Advanced Model
+#Apply s transformation to media data
+a_tv<-200
+b_tv<-0.00001
+tv_s_fun<-function(x){a_tv*x/(1+b_tv*x)}
+plot(tv_s_fun(1:3000000),type = "l")
+
+a_dig<-150
+b_dig<-0.000001
+dig_s_fun<-function(x){a_dig*x/(1+b_dig*x)}
+plot(dig_s_fun(1:2000000),type = "l")
+
+RawData$tv_s<-lapply(RawData$tv_spend,tv_s_fun)
+RawData$dig_s<-lapply(RawData$digital_spend,dig_s_fun)
+
+#apply ad-stock
+tv_ads<-0.1
+dig_ads<-0.001
+RawData$tv_s_ads<-as.numeric(filter(x=RawData$tv_s,filter = tv_ads, method = "recursive"))
+RawData$dig_s_ads<-as.numeric(filter(x=RawData$dig_s,filter = dig_ads, method = "recursive"))
+
+Model2<-lm(sales ~ tv_s_ads + dig_s_ads +trend + xmas-1,RawData)
 summary(Model2)
 plot(Model2, which = c(1, 2))
+
+VisData$Fitted2<-predict(Model2)
+ggplot(VisData,aes(month)) +
+  geom_line(aes(y=sales)) +
+  geom_line(aes(y=Fitted2,col="Predicted"))
+
+#TV contribution for last two years
+TvSale2<-sum(RawData$tv_s_ads*coef(Model2)[1])
+TotPreSal2<-sum(VisData$Fitted2)
+TvPer2<-TvSale2/TotPreSal2
+TvROI2<-TvSale2/sum(VisData$tv_spend)
+
+#Plan for 2018, asumming trend has stopped
+PlannedData$trend<-24
+PlannedData$xmas<-0
+PlannedData$tv_s<-lapply(PlannedData$tv_spend,tv_s_fun)
+PlannedData$dig_s<-lapply(PlannedData$digital_spend,dig_s_fun)
+PlannedData$tv_s_ads<-as.numeric(filter(x=PlannedData$tv_s,filter = tv_ads, method = "recursive"))
+PlannedData$dig_s_ads<-as.numeric(filter(x=PlannedData$dig_s,filter = dig_ads, method = "recursive"))
+predict(Model2, PlannedData, interval="predict") 
+
+###Compare spend with transformed spend
+#ggplot(RawData, aes(month)) + 
+#  geom_line(aes(y = tv_spend, colour = "TV Investment")) +
+#  geom_line(aes(y = tv_s_ads, colour = "TV trans")) 
 
